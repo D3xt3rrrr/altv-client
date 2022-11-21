549973f1370f7d50236348a4ac99a6e1d14a6e71
D3xt3rr <mathieu.thibeault-thuot@outlook.com>
2022-11-21 11:59:40 -0500

    vue

---
+++ Webview/lscustom/src/components/DashBoard.vue
@@ -0,0 +1,20 @@
+<template>
+  <CardMod></CardMod>
+</template>
+
+<script>
+import CardMod from "@/components/CardMod";
+export default {
+  name: 'DashBoard',
+  components: {
+    CardMod
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
